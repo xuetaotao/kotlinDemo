f5615694f58613839b4372ec8ab444e4c3b28d1c
xuetaotao <xuetaotao@xgd.com>
2021-03-29 18:01:49 +0800

    feat：新增ExpandableListView

--- app/src/main/java/com/jlpay/kotlindemo/ui/main/AdapterViewActivity.kt
+++ app/src/main/java/com/jlpay/kotlindemo/ui/main/AdapterViewActivity.kt
@@ -3,6 +3,7 @@ package com.jlpay.kotlindemo.ui.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -42,48 +43,80 @@ class AdapterViewActivity : AppCompatActivity() {
      *
      */
     fun expandable() {
-        val expandableListAdapter: ExpandableListAdapter = object : BaseExpandableListAdapter() {
+        val logos: IntArray =
+            intArrayOf(R.mipmap.ic_launcher, R.mipmap.zhizhuxia, R.mipmap.hanfei)
+        val armTypes = arrayOf("letter", "number", "characters")
+        val arms = arrayOf(
+            arrayOf("AA", "BB", "CC"),
+            arrayOf("11", "22", "33"),
+            arrayOf("嗯嗯", "好呢", "是的")
+        )
 
-            var logos: IntArray = intArrayOf(R.mipmap.zhizhuxia, R.mipmap.dog, R.mipmap.baby)
-            var armTypes = arrayOf("letter", "number", "characters")
-            var arms = arrayOf(
-                arrayOf("AA", "BB", "CC"),
-                arrayOf("11", "22", "33"),
-                arrayOf("嗯嗯", "好呢", "是的")
-            )
+        val expandableListAdapter: ExpandableListAdapter = object : BaseExpandableListAdapter() {
 
             //获取指定组位置处的组数据
             override fun getGroup(groupPosition: Int): Any {
                 return armTypes[groupPosition]
             }
 
+            //指定位置上的子元素是否可选中
             override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
-                TODO("Not yet implemented")
+                return true
             }
 
+            //分组和子选项是否持有稳定的ID, 就是说底层数据的改变会不会影响到它们
             override fun hasStableIds(): Boolean {
-                TODO("Not yet implemented")
+                return true
             }
 
+            /**
+             * 获取显示指定分组的视图
+             *
+             * @param groupPosition 组位置
+             * @param isExpanded    该组是展开状态还是伸缩状态，true=展开
+             * @param convertView   重用已有的视图对象
+             * @param parent        返回的视图对象始终依附于的视图组
+             * @return
+             */
             override fun getGroupView(
                 groupPosition: Int,
                 isExpanded: Boolean,
                 convertView: View?,
                 parent: ViewGroup?
             ): View {
-                TODO("Not yet implemented")
+                lateinit var ll: LinearLayout
+                val viewHolder: ViewHolder
+                if (convertView == null) {
+                    ll = LinearLayout(this@AdapterViewActivity)
+                    ll.orientation = LinearLayout.HORIZONTAL
+                    val imageView: ImageView = ImageView(this@AdapterViewActivity)
+                    ll.addView(imageView)
+                    val textView: TextView = TextView(this@AdapterViewActivity)
+                    ll.addView(textView)
+                    viewHolder = ViewHolder(imageView, textView)
+                    ll.tag = viewHolder
+
+                } else {
+                    ll = convertView as LinearLayout
+                    viewHolder = ll.tag as ViewHolder
+                }
+
+                viewHolder.imageView.setImageResource(logos[groupPosition])
+                viewHolder.textView.text = armTypes[groupPosition]
+                return ll
             }
 
             override fun getChildrenCount(groupPosition: Int): Int {
-                TODO("Not yet implemented")
+                return arms[groupPosition].size
             }
 
+            //获取指定组位置、指定子列表项处的子列表项数据
             override fun getChild(groupPosition: Int, childPosition: Int): Any {
-                TODO("Not yet implemented")
+                return arms[groupPosition][childPosition]
             }
 
             override fun getGroupId(groupPosition: Int): Long {
-                TODO("Not yet implemented")
+                return groupPosition.toLong()
             }
 
             override fun getChildView(
@@ -93,21 +126,69 @@ class AdapterViewActivity : AppCompatActivity() {
                 convertView: View?,
                 parent: ViewGroup?
             ): View {
-                TODO("Not yet implemented")
+                lateinit var textView: TextView
+                if (convertView == null) {
+                    textView = TextView(this@AdapterViewActivity)
+                    val layoutParams: AbsListView.LayoutParams = AbsListView.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                    textView.layoutParams = layoutParams
+                    //按位与：Kotlin(and)，等价于Java(&)
+                    //按位或：Kotlin(or)，等价于Java(|)
+                    //按位异或：Kotlin(xor)，等价于Java(异或)
+                    textView.gravity = Gravity.CENTER_VERTICAL or Gravity.START
+                    textView.setPadding(36, 10, 0, 10)
+                    textView.textSize = 18f
+
+                } else {
+                    textView = convertView as TextView
+                }
+
+                textView.text = getChild(groupPosition, childPosition).toString()
+                return textView
             }
 
             override fun getChildId(groupPosition: Int, childPosition: Int): Long {
-                TODO("Not yet implemented")
+                return childPosition.toLong()
             }
 
             override fun getGroupCount(): Int {
-                TODO("Not yet implemented")
+                return armTypes.size
             }
         }
 
-
         val expandableListView: ExpandableListView = findViewById(R.id.expandableListView)
-//        expandableListView.setAdapter()
+        expandableListView.setAdapter(expandableListAdapter)
+        expandableListView.setOnChildClickListener(object :
+            ExpandableListView.OnChildClickListener {
+            override fun onChildClick(
+                parent: ExpandableListView?,
+                v: View?,
+                groupPosition: Int,
+                childPosition: Int,
+                id: Long
+            ): Boolean {
+                Toast.makeText(
+                    this@AdapterViewActivity,
+                    armTypes[groupPosition] + "：" + arms[groupPosition][childPosition],
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            }
+        })
+    }
+
+
+    /**
+     * 内部类：定义在类内部的类，与类成员有相似的访问控制；Kotlin默认是静态内部类，非静态类用inner关键字，this@outter,this@inner的用法
+     * 菲静态内部类，持有外部类的引用，可以访问到外部类的成员变量(实例化也需要先new 外部类)；静态内部类不能访问外部类的成员变量；
+     * 匿名内部类：没有定义名字的内部类，类名编译时产生，类似 OuterClass$1.class
+     * 在java中，匿名内部类就能继承一个类，不能实现接口
+     * kotlin中匿名内部类，就和普通的类一样，可以继承一个类，实现多个接口
+     */
+    class ViewHolder(var imageView: ImageView, var textView: TextView) {
+
     }
 
 

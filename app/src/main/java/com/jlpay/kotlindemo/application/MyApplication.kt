15b38c4b9b62a51474ed1f2544a755b06cb37185
xuetaotao <xuetaotao@xgd.com>
2021-04-23 17:59:59 +0800

    fix：完善Tinker热更新

--- app/src/main/java/com/jlpay/kotlindemo/application/MyApplication.kt
+++ app/src/main/java/com/jlpay/kotlindemo/application/MyApplication.kt
@@ -5,6 +5,8 @@ import android.app.Application
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
 import androidx.multidex.MultiDex
 import com.jlpay.kotlindemo.ui.base.Constants
 import com.jlpay.kotlindemo.ui.utils.AppUtils
@@ -70,9 +72,34 @@ class MyApplication(
     private fun initLib() {
         AppUtils.initAppUtils(mContext)
 
+        initBugly()
+    }
+
+
+    private fun initBugly() {
+        setStrictMode()
+        // 设置是否开启热更新能力，默认为true
+        Beta.enableHotfix = true
+        // 设置是否自动下载补丁
+        Beta.canAutoDownloadPatch = true
+        // 设置是否提示用户重启
+        Beta.canNotifyUserRestart = true
+        // 设置是否自动合成补丁
+        Beta.canAutoPatch = true
         // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId。调试时，将第三个参数改为true
         Bugly.init(application, Constants.BUGLY_APP_ID, false)
     }
 
 
+    @TargetApi(9)
+    protected fun setStrictMode() {
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+        StrictMode.setVmPolicy(VmPolicy.Builder().detectAll().penaltyLog().build())
+    }
+
+    //程序终止的时候执行
+    override fun onTerminate() {
+        super.onTerminate()
+        Beta.unInit()
+    }
 }

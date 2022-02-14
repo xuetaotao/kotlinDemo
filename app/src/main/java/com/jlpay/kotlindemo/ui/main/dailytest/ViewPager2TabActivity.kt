254467bc073eb73885ea00b0d32c9bea7e9f7258
xuetaotao <xuetaotao@xgd.com>
2022-02-14 18:27:26 +0800

    feat:ViewPager2的剩余代码提交

--- app/src/main/java/com/jlpay/kotlindemo/ui/main/dailytest/ViewPager2TabActivity.kt
+++ app/src/main/java/com/jlpay/kotlindemo/ui/main/dailytest/ViewPager2TabActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.jlpay.kotlindemo.R
 import com.jlpay.kotlindemo.databinding.ActivityViewpager2TabBinding
-import com.jlpay.kotlindemo.databinding.ItemTabLayoutBinding
 
 class ViewPager2TabActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityViewpager2TabBinding
-    private lateinit var fragments: ArrayList<Fragment>
+    private val fragments: ArrayList<Fragment> by lazy { ArrayList() }
     private val tabTitles: List<String> = arrayListOf<String>("微信", "通讯录", "发现", "我")
+    private val viewPager2Adapter by lazy { MyViewPager2Adapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,24 +30,63 @@ class ViewPager2TabActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        fragments = ArrayList()
         for (tabTitle in tabTitles) {
-            fragments.add(LifeCycleFragment.newInstance(tabTitle))
-            //为TabLayout添加tab,也可以在xml布局中以 TabItem 方式添加
-//            mBinding.tabLayout.addTab(mBinding.tabLayout.newTab().setText(tabTitle))
-            val tabCustomView = DataBindingUtil.inflate<ItemTabLayoutBinding>(
-                layoutInflater,
-                R.layout.item_tab_layout,
-                mBinding.tabLayout,
-                false
-            )
-            tabCustomView.tvContent.text = tabTitle
-            val newTab = mBinding.tabLayout.newTab()
-            newTab.customView = tabCustomView.root
-            mBinding.tabLayout.addTab(newTab)
+            //方式一：为TabLayout添加tab,也可以在xml布局中以 TabItem 方式添加
+            mBinding.tabLayout.addTab(mBinding.tabLayout.newTab().setText(tabTitle))
+            //方式二：添加自定义View
+//            val tabCustomView = DataBindingUtil.inflate<ItemTabLayoutBinding>(
+//                layoutInflater,
+//                R.layout.item_tab_layout,
+//                mBinding.tabLayout,
+//                false
+//            )
+//            tabCustomView.tvContent.text = tabTitle
+//            val newTab = mBinding.tabLayout.newTab()
+//            newTab.customView = tabCustomView.root
+//            mBinding.tabLayout.addTab(newTab)
         }
 
-//        mBinding.tabLayout.addOnTabSelectedListener(object :
-//            TabLayout.ViewPagerOnTabSelectedListener(mBinding.viewpager2))
+        //给ViewPager设置adapter
+        mBinding.viewpager2.adapter = viewPager2Adapter
+
+        //设置从 viewpager 到 tabLayout 的关联滑动
+        TabLayoutMediator(mBinding.tabLayout, mBinding.viewpager2) { tab, position ->
+            //TODO 自定义的View不知道如何修改
+            tab.text = tabTitles[position]
+        }.attach()
+
+        //设置从 tabLayout 到 viewpager 的关联滑动
+        val onTabSelectedListener = object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    mBinding.viewpager2.currentItem = it.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        }
+        mBinding.tabLayout.addOnTabSelectedListener(onTabSelectedListener)
+    }
+
+    inner class MyViewPager2Adapter : FragmentStateAdapter(this) {
+
+        init {
+            fragments.clear()
+            for (tabTitle in tabTitles) {
+                fragments.add(LifeCycleFragment.newInstance(tabTitle))
+            }
+        }
+
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment {
+            return LifeCycleFragment.newInstance(tabTitles[position])
+        }
     }
 }
